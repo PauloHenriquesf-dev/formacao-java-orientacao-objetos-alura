00b274ffe5e9fd92fd3c3055f9b67fd79db1a0fa
PauloHenriquesf-dev <paulohenriquesf97@gmail.com>
2023-12-29 14:05:20 -0300

    Adicionando aula02-JRE-JDK-JVM: Exploração dos componentes essenciais do JRE, JDK e JVM.

---
+++ mod1-JREeJDK/aula02-JRE-JDK-JVM/src/Aula02.java
@@ -0,0 +1,18 @@
+
+public class Aula02 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * Nessa aula, discutimos os principais componentes do Java Runtime Environment (JRE),
+		 * do Java Development Kit (JDK) e da Java Virtual Machine (JVM),
+		 * abordando suas funções e como trabalham em conjunto para permitir o desenvolvimento,
+		 * compilação e execução de aplicações Java.
+		 * 
+		 * Tem o pdf com tudo explicado na pasta: explicacao-JRE-JDK-JVM
+		 * 
+		 */
+		
+	}
+	
+}
