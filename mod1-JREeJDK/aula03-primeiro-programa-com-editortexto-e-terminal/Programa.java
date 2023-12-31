67e80fa03abb8120b6b6ea610fb21e9c35c87ebc
PauloHenriquesf-dev <paulohenriquesf97@gmail.com>
2023-12-31 04:30:55 -0300

    Adicionando aula03: criando, compilando e executando programa usando editor de texto e terminal

---
+++ mod1-JREeJDK/aula03-primeiro-programa-com-editortexto-e-terminal/Programa.java
@@ -0,0 +1,16 @@
+public class Programa {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Olá Mundo!");
+
+		/*
+		 * Tem a descrição, passo a passo de como criar, compilar e executar
+		 * esse primeiro programa usando o editor de texto e o terminal
+		 * basta ir na pasta "como-criar-programa-com-editortexto-e-terminal" 
+		 *
+		 */
+
+	}
+
+}
